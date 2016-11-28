d52053afef9bd92392c21e36db61ce2aa9009e9a
kairihye <hyei@uw.edu>
2016-11-28 09:43:41 -0800

    working on email input testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import SignUpForm from './TeamSignUp';
+import {shallow} from 'enzyme';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
+
+describe('<EmailInput />', () => {
+ let wrapper;
+
+ beforeEach(() => {
+   wrapper = shallow(<EmailInput />);
+ });
+
+ it('should be a required field', () => {
+   const input = wrapper.find('input');
+   input.simulate('change', {target:{value:''}});
+   expect(wrapper.find('p .help-block')).hasClass('error-missing');
+
